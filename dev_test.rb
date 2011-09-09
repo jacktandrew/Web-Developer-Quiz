6abd6dc4b21342ba7b2d5a57ed6ff67f300e6403
Jack Andrew <jacktandrew@gmail.com>
2011-09-09 11:49:39 -0500

    improve test add to README

--- dev_test.rb
+++ dev_test.rb
@@ -1,15 +1,23 @@
 puts 'A test of web developer knowledge!',
      '\n'
 
-@n = 1                      # a variable that counts and displays which number you are on for the Flashcard Test
-@explain = {}               # a hash stores the questions and explainations for the Flashcard Test 
-@input = ""                 # a variable is used throughout both tests for user input
-@correct = 0                # a variable that counts the correct answers for both tests
-@one_test = []              # a array that will be populated with questions from @all_the_tests
-@c = 0                      # a variable that counts the multiple choices answers starting at 0 and going to 3
-@multi_choice_array = []    # an empty array that will be filled with multiple 4 choice answers
+@n = 1                      # A variable that counts and displays which number you are on for the Flashcard Test
+@explain = {}               # A hash stores the questions and explainations for the Flashcard Test 
+@input = ""                 # A variable is used throughout both tests for user input
+@correct = 0                # A variable that counts the correct answers for both tests
+@one_test = []              # A array that will be populated with questions from @all_the_tests
+@c = 0                      # A variable that counts the multiple choices answers starting at 0 and going to 3
+@multi_choice_array = []    # An empty array that will be filled with multiple 4 choice answers
 
-@all_the_tests = [          # an array that contains all three tests
+@all_the_tests = [          # An array that contains all three tests
+                            # A symbol or tag at [0]
+                            # The name of the symbol or tag [1] 
+                            # A description of the at [2]
+                            # and a sub-array of keywords [3] 
+                            # Later I may make a function to evalute the 
+                            # Explainations but I've left that for another day
+                            # for now [3] is not used
+
   [                         # a sub-array containing all the Regexp_questions 
     ['?','question mark','? means optional', ['optional', 'optional'] ],
     ['+','plus sign','+ means one or more', ['one', 'more'] ],
@@ -22,9 +30,9 @@ puts 'A test of web developer knowledge!',
     ['A-Z','range','character classes can include ranges', ['can', 'range'] ],
     ['0-9','range','character classes can include ranges', ['can', 'range'] ],
     ['^','caret','^ can negate a character class', ['negate', 'negate'] ],
-    ['\\D','D for digit','capital negate! everything BUT digits', ['capital', 'negate', 'digit'] ],
-    ['\\S','S for space','capital negate! everything BUT spaces', ['capital', 'negate', 'space'] ],
-    ['\\W','W for word','capital negate! everything BUT words', ['capital', 'negate', 'word'] ],
+    ['\\D','D for digit','Everything BUT digits because capital negate!', ['capital', 'negate', 'digit'] ],
+    ['\\S','S for space','Everything BUT spaces because capital negate!', ['capital', 'negate', 'space'] ],
+    ['\\W','W for word','Everything BUT words because capital negate!', ['capital', 'negate', 'word'] ],
     ['\\A','A for anchor','\\A anchors the start of a string', ['anchor', 'start', 'string'] ],
     ['\\z','z for anchor','\\z anchors the end of a string', ['anchor', 'end', 'string'] ],
     ['/^','caret','/^ anchors the start of lines', ['anchor', 'start', 'line'] ],
@@ -72,7 +80,7 @@ puts 'A test of web developer knowledge!',
     ['!','bang','indicates not in "!=" not equal among other things...'],
     ['_','underscore','used in place of "space" to divid words'],
     ['#','hash','placed before colors in hexidecimal AND shorthand for <div id=...> in CSS among other things...'],
-    ['<!--','comment','use this ONLY in HTML'],
+    ['<!--','comment','use this ONLY in HTML to write a comment'],
     ['//','comment','use this ONLY in CSS or JavaScript'],
     ['&','ampersand','used before strange characters'],
     ['+','plus sign','used in math AND to concatenate strings']
@@ -85,29 +93,29 @@ def choose_a_test
   c_input = choice_input[0]                         # Only the first letter of user input is stored 
                                                      
   if c_input == 'R'                                 # If only checks the string's 1st letter was R 
-    @questions = @all_the_tests[0]                  # the chosen sub-array is pull by it's index number
-    subject = "RegExp"                              # a string is supplied for the title
+    @one_test = @all_the_tests[0]                   # The chosen sub-array is pull by it's index number
+    subject = "RegExp"                              # A string is supplied for the title
     
   elsif c_input == 'H'                              # 'h', 'H', 'HTML' or even 'Hello' are all 'true'
-    @questions = @all_the_tests[1]
+    @one_test = @all_the_tests[1]
     subject = "HTML"
   
   elsif c_input == 'S'
-    @questions = @all_the_tests[2]
+    @one_test = @all_the_tests[2]
     subject = "Symbols"
   else                                              # Invalid input will result in an 
-    puts "I didn't understand..."                   # an easy to read error message
+    puts "I didn't understand..."                   # An easy to read error message
     choose_a_test()                                 # and the function is called again
   end
 
   puts 'Do you want (F)lashcards or (M)ultiple-Choice'
   print '> '
   choice_input = gets.chomp.capitalize              
-  c_input = choice_input[0]                         # again only the first letter of a input string is evaluated
+  c_input = choice_input[0]                         # Again only the first letter of a input string is evaluated
   
   if c_input == 'F'
     form = 'Flash Card Test'                                            
-    puts "", "<>{ () }<>  " + subject + ' ' + form + " <>{ () }<>", ""  # a decorative title bar is created
+    puts "", "<>{ () }<>  " + subject + ' ' + form + " <>{ () }<>", ""  # A decorative title bar is created
     flashcard_test()
   elsif c_input == 'M'
     form = 'Multiple Choice Test'
@@ -131,29 +139,33 @@ end
 
 def info_for_multi_choice_test
   puts "", "Answer 1, 2, 3, or 4 for each question"
+  puts "If you see 2 correct answers,"
+  puts "choose the one with the lower number."
   puts "Type 'quit' to end the test early" 
 end
 
 def grab_a_question                                   # Called by the Flashcard Test
   if @one_test.length >= 1                            # A checks to see if any questions have yet to be asked
     random_number = rand(@one_test.length)            # Random number is generated 
-    @aQuestion = @questions.slice!(random_number)     # 1 question is removed from the array at random and returned
+    @aQuestion = @one_test.slice!(random_number)      # 1 question is removed from the array at random and returned
     pose_a_question()                                 # The next function is called to pose the selected question
   end
 end
 
-def grab_many_questions                               # Called by the Multiple Choice Test
-#  @Q_n_A_array = []           # an empty array that will be filled array containing both questions and answers
-random_number = ""  
-  while @n < 20                                       # Runs the function 20 times
-    while @c < 4                                      # Runs the loop 4 times
-      random_number = rand(@one_test.length)          # Generates a random number based on the total # of questions
-      @Q_n_A_array = @one_test.slice(random_number)    # Picks 1 array from the larger array
-      answer_value = @Q_n_A_array[0]                   # Pulls 1st value which is one the multiple choice
-      question_value = @Q_n_A_array[2]                 # Pulls 3rd value which is the question
-      @multi_choice_array.push(question_value)        # Questions enter the array in 0th, 2nd, 4th, & 6th places
-      @multi_choice_array.push(answer_value)          # Answers enter the array in 1st, 3rd, 5th, & 7th places
-      @c += 1
+def grab_many_questions       # Called by the Multiple Choice Test
+  @S_n_D_array = []           # Array that will be filled array containing 1 symbol and 1 description
+  @multi_choice_array = []    # Array will be filled with 4 questions and 4 answers
+  random_number = ""          # Random number between 0 and the length of the test
+  q = 0                       # Counts how many symbol and description pairs have been pulled out
+  while @n < 20                                   # Runs the function 20 times
+    while q < 4                                   # Runs the loop 4 times so 4 answer choices will appear
+      random_number = rand(@one_test.length)
+      @S_n_D_array = @one_test.slice(random_number) # Picks 1 array from the larger array
+      symbol = @S_n_D_array[0]                      # Pulls 1st value which is one the multiple choice
+      description = @S_n_D_array[2]                 # Pulls 3rd value which is the question
+      @multi_choice_array.push(description)         # Questions enter the array in 0th, 2nd, 4th, & 6th places
+      @multi_choice_array.push(symbol)              # Answers enter the array in 1st, 3rd, 5th, & 7th places
+      q += 1
     end
     @n += 1
     pose_a_multi_choice_question()
@@ -162,23 +174,23 @@ end
 
 
 def pose_a_question
-  print "#{@n}. \s \s" + @aQuestion.first + "\s \s > "
-  @n += 1
-  get_flashcard_input()
-  @explain[@aQuestion[0]] = @input
-  puts "Your answer should be something like...", " -> #{@aQuestion[1]}"
+  print "#{@n}. \s \s" + @aQuestion.first + "\s \s > "  # The question number and question are printed
+  @n += 1                                               # The number counter is increased by 1
+  get_flashcard_input()                                 # The user input is process and returned
+  puts "Your answer should be something like...",       # 
+       " -> #{@aQuestion[1]}"                           # The stock answer is displayed
   get_explaination()
 end
 
 def pose_a_multi_choice_question
-  print "(1) #{@multi_choice_array[1]} \s\s\t"    # The questions and answers entered the array as pairs
-  print "(2) #{@multi_choice_array[3]} \s\s\t"    # the answers came second and so they occupy the odd 
-  print "(3) #{@multi_choice_array[5]} \s\s\t"    # index places of the multi_choice_array
-  print "(4) #{@multi_choice_array[7]} \n"
+  print "\n", "(1) #{@multi_choice_array[1]} \s\s\t"  # The questions and answers entered the array as pairs
+  print "(2) #{@multi_choice_array[3]} \s\s\t"        # The answers came second and so they occupy the odd 
+  print "(3) #{@multi_choice_array[5]} \s\s\t"        # Index places of the multi_choice_array
+  print "(4) #{@multi_choice_array[7]} \n \n"
 
-  random_number = rand(4)                 # Random number is equal to 0, 1, 2, or 3. 
-  even_number = 2 * random_number         # Multiplied by 2 makes it even 0, 2, 4, or 6 and so it will select only a question
-  human_number = random_number + 1        # Plus 1 makes the number understandable to for users
+  number_1_to_4 = rand(4)                 # Random number is equal to 0, 1, 2, or 3. 
+  even_number = 2 * number_1_to_4         # Multiplied by 2 makes it even 0, 2, 4, or 6 and so it will select only a question
+  human_number = number_1_to_4 + 1        # Plus 1 makes the number understandable to for users
   string_number = human_number.to_s       # Converting to a string allows it to be compared to the user input
   print "Q: ",                            # A question prompt is printed
     @multi_choice_array[even_number],     # A questions is printed at random
@@ -196,73 +208,83 @@ def pose_a_multi_choice_question
 end                                       # Causing the a new cycle to begin
 
 def get_multi_choice_input
-  @input = gets.chomp
-  if @input == 'quit'
-    @n -= 1
-    finish_test()
-  elsif @input == 'skip'
-    grab_many_question()
-  else
+  @input = gets.chomp                     # Asks user for input
+  if @input == 'quit'                     # Allows user to exit
+    @n -= 1                               # Decreases counter by one so last question is not counted against the user
+    finish_test()                         
+  elsif @input == 'skip'                  # Allows user to skip the question
+    grab_many_question()                  # Goes back a grabs a new question
+  else 
     return @input
   end
 end
 
 def get_flashcard_input
-  @input = gets.chomp
-  if @input == 'quit'
-    @n -= 1
-    finish_test()
-  elsif @input == 'skip'
-    grab_a_question()
-  elsif @input == 'help'
-    info_for_flashcard_test()
-  else
-    @explain[@aQuestion[0]] = @input
-  end
-end
+  @input = gets.chomp                     # Asks for user input store in @input variable
+  if @input == 'quit'                     # Allows the user to end the test early
+    @n -= 1                               # Decreases counter by one so last question is not counted against the user
+    finish_test()                         #
+  elsif @input == 'skip'                  # Allows user to skip the question
+    grab_a_question()                     # Goes back a grabs a new question
+  elsif @input == 'help'                  # Allows the user to reread the instructions
+    info_for_flashcard_test()             # Prints the instructions again
+  else                                    #
+    @explain[@aQuestion[0]] = @input      # Stores the user's explaination in a hash called @explain
+    return @input                         #
+  end                                     # The question is the 'key' of the hash
+end                                       # and the user input is the 'value' of the hash
 
 def get_explaination
-  puts "Please write an explaination : "
+  puts "Please write an explaination : "                     # Asks the user for input
   print "> "
-  get_flashcard_input()
-  puts "Your explaination should be something like...", " -> #{@aQuestion[2]}"
-  self_correct()
-  grab_a_question()
+  get_flashcard_input()                                      # Evaluates input to check if user wants help, to skip, or quit 
+  puts "Your explaination should be something like...",      # Provides an example explainations
+       "\n",
+       " -> #{@aQuestion[2]}"
+  self_correct()                                             # Give the user a chance to self-correct
 end
 
 def self_correct
-  print "correct?  y / n  ", " > "
-  self_correct = gets.chomp
-  if self_correct == 'y'
-    puts "good job!", ""
-    @correct += 1
+  print "\n", "correct?  y / n  ", " > "                     # Asks the user if his/her answer was correct
+  self_correct = gets.chomp                                  # Receives the user input
+  if self_correct == 'y'                                     # And evaluates
+    puts "good job!", "\n"                                   # Print 'good job' 
+    @correct += 1                                            # Increase correct counter by 1
   elsif self_correct == 'n'
-    @questions.push(@aQuestion)
-    print "ok we put that one back so you can try it again later",""
+    @one_test.push(@aQuestion)                               # If 'n' question pushed back into the test
+    print "\n", 
+          "We'll put that one back so you can try it again later \n",
+          "\n"
   else
     puts "sorry I didn't understand that..."
-    self_correct()
+    self_correct()                                           # Catches bad input asks the question again
   end
+  grab_a_question()                                          # Goes back a grabs a new question
 end
 
 def finish_test
-  @n -= 1
+  @n -= 1                                                    # Decreases the number counter by one so last question is iqnored
   results = "Finished! You got #{@correct} out of #{@n} answers right!"
-  puts results
-  puts "Do you want to save these results?  y / n"
-  want_to_save = gets.chomp
-  if want_to_save == 'y'  
-    @explain['results'] = results
-    the_time = Time.now.to_i
-    filename = "answers-" + the_time.to_s + ".rb"
-    target = File.open(filename, 'w')
-    target.write(@explain)
-    target.close()
-    puts "ok, your results were saved"
+  puts results                                               # Results are printed for the user
+  if @explain.length < 1                                     # Multiple-Choice doens't have explainations so @explain hash is 0 
+    Process.exit!(0)                                         # Multiple-Choice users are exited from the program
   else
-    puts "ok, your results were erased"
+    puts "Do you want to save these results?  y / n"         # Flashcard users are asked if the want to save results
+    want_to_save = gets.chomp
+    if want_to_save == 'y'  
+      @explain['results'] = results                          # 1 more hash value is add containing the results
+      the_time = Time.now.to_i                               # Current time detected and decimal of seconds removed
+      filename = "answers-" + the_time.to_s + ".rb"          # Time is included into the filename so each is unique
+      folder = Dir.chdir('results')                          # Directory is changed to the 'results' directory
+      target = File.open(filename, 'w')                      # A file is created in the results folder
+      target.write(@explain)                                 # @explain hash is written to file
+      target.close()                                         # and the file is closed
+      puts "ok, your results were saved"
+    else
+      puts "ok, your results were erased"
+    end
   end
-  Process.exit!(0)
+  Process.exit!(0)                                           # User is exited from the program
 end
 
 def flashcard_test
